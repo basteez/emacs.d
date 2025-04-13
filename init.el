afd498998c1094ed1108d4e8e6d3c0705c8700be
Tiziano Basile <tiz.basile@gmail.com>
2025-04-13 15:13:56 +0200

    first working conf

---
+++ init.el
@@ -0,0 +1,76 @@
+(use-package package
+  :config
+  (add-to-list 'package-archives
+               '("melpa" . "https://melpa.org/packages/"))
+  (package-initialize))
+
+(use-package use-package
+  :custom
+  (use-package-always-ensure t)
+  (package-native-compile t)
+  (warning-minimum-level :error))
+
+;;; Personal configuration -*- lexical-binding: t -*-
+
+;; Save the contents of this file under ~/.emacs.d/init.el
+;; Do not forget to use Emacs' built-in help system:
+;; Use C-h C-h to get an overview of all help commands.  All you
+;; need to know about Emacs (what commands exist, what functions do,
+;; what variables specify), the help system can provide.
+
+;; Load a custom theme
+(load-theme 'misterioso t)
+
+;; Use whatever the default monospace font is
+(setq font-use-system-font t)
+
+;; Disable the menu bar
+(menu-bar-mode -1)
+
+;; Disable the tool bar
+(tool-bar-mode -1)
+
+;; Disable the scroll bars
+(scroll-bar-mode -1)
+
+;; Disable splash screen
+(setq inhibit-startup-screen t)
+
+;; Enable line numbering by default
+(global-display-line-numbers-mode t)
+
+;; Automatically pair parentheses
+(electric-pair-mode t)
+
+;; Miscellaneous options
+(setq-default major-mode
+              (lambda () ; guess major mode from file name
+                (unless buffer-file-name
+                  (let ((buffer-file-name (buffer-name)))
+                    (set-auto-mode)))))
+;;(setq confirm-kill-emacs #'yes-or-no-p)
+(setq window-resize-pixelwise t)
+(setq frame-resize-pixelwise t)
+(save-place-mode t)
+(savehist-mode t)
+(recentf-mode t)
+(defalias 'yes-or-no #'y-or-n-p)
+
+;; Store automatic customisation options elsewhere
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+;; CUSTOM
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory "~/Documents/orgVault")
+  (org-roam-completion-everywhere t)
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+	 ("C-c n f" . org-roam-node-find)
+	 ("C-c n i" . org-roam-node-insert)
+	 :map org-mode-map
+	 ("C-M-i"   . completion-at-point))
+  :config
+  (org-roam-setup))
