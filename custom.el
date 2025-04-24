4868ada17ac3c61b3530833ee356eaac92f5e656
tbasile <tiziano.basile@team.blue>
2025-04-24 15:14:31 +0200

    feat: move custom config in custom.el

---
+++ custom.el
@@ -0,0 +1,13 @@
+;; CUSTOM
+;(use-package org-roam
+;  :ensure t
+;  :custom
+;  (org-roam-directory "~/Documents/orgVault")
+;  (org-roam-completion-everywhere t)
+;  :bind (("C-c n l" . org-roam-buffer-toggle)
+;         ("C-c n f" . org-roam-node-find)
+;         ("C-c n i" . org-roam-node-insert)
+;         :map org-mode-map
+;         ("C-M-i"   . completion-at-point))
+;  :config
+;  (org-roam-setup))
--- init.el
+++ init.el
@@ -60,17 +60,3 @@
 (setq custom-file (locate-user-emacs-file "custom.el"))
 (when (file-exists-p custom-file)
   (load custom-file))
-
-;; CUSTOM
-(use-package org-roam
-  :ensure t
-  :custom
-  (org-roam-directory "~/Documents/orgVault")
-  (org-roam-completion-everywhere t)
-  :bind (("C-c n l" . org-roam-buffer-toggle)
-	 ("C-c n f" . org-roam-node-find)
-	 ("C-c n i" . org-roam-node-insert)
-	 :map org-mode-map
-	 ("C-M-i"   . completion-at-point))
-  :config
-  (org-roam-setup))
