4b62ded043e217d4d4dc8e85c917cf9a583ea9a1
Tiziano Basile <tiz.basile@gmail.com>
2025-04-24 16:16:37 +0200

    feat: add few options

--- init.el
+++ init.el
@@ -48,13 +48,30 @@
                 (unless buffer-file-name
                   (let ((buffer-file-name (buffer-name)))
                     (set-auto-mode)))))
+
 ;;(setq confirm-kill-emacs #'yes-or-no-p)
 (setq window-resize-pixelwise t)
 (setq frame-resize-pixelwise t)
-(save-place-mode t)
-(savehist-mode t)
-(recentf-mode t)
 (defalias 'yes-or-no #'y-or-n-p)
+
+;; remember and restore the last cursor location of opened files
+(save-place-mode t)
+
+;; remember commands history
+(setq history-length 25)
+(savehist-mode t)
+
+;; remember recent edited files
+(recentf-mode t)
+
+;; Don't pop up UI dialogs when prompting
+(setq use-dialog-box nil)
+
+;; Autorefresh buffers when related file is changed
+(global-auto-revert-mode 1)
+
+;; Autorefresh Dired and other buffers
+(setq global-auto-revert-non-file-buffers t)
 
 ;; Store automatic customisation options elsewhere
 (setq custom-file (locate-user-emacs-file "custom.el"))
