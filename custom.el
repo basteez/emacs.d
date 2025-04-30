0799113a83d02af0dea7ec4b25634d3fb3a2b9c6
tbasile <tiziano.basile@team.blue>
2025-04-30 16:06:38 +0200

    feat: add magit

--- custom.el
+++ custom.el
@@ -23,6 +23,11 @@
   :config
   (org-roam-setup))
 
+;; Open daily journal on startup
+;(add-hook 'emacs-startup-hook
+;          (lambda ()
+;(org-roam-dailies-capture-today)))
+
 ;; --- Org-roam dailies ---
 (setq org-roam-dailies-capture-templates
       '(("d" "default" entry
@@ -86,7 +91,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(consult-org-roam org-roam-ui org-modern)))
+ '(package-selected-packages '(magit consult-org-roam org-roam-ui org-modern)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
